e1fff0f39e6749105491f1e6b3c96332cdfe708c
YufaZhu <412167563@qq.com>
2020-03-10 21:06:32 +0800

    cascaderupdate

--- src/pages/product/add-update.jsx
+++ src/pages/product/add-update.jsx
@@ -1,38 +1,57 @@
 import React, { Component } from 'react';
-import {Card,Form,Input,Cascader,Upload,Button,Icon} from 'antd'
+import { Card, Form, Input, Cascader, Upload, Button, Icon } from 'antd'
 import LinkButton from './../../components/link-button'
-import {reqCategorys} from './../../api'
+import { reqCategorys } from './../../api'
 
-const {Item} = Form
-const {TextArea} = Input
+const { Item } = Form
+const { TextArea } = Input
 
 class ProductAddUpdate extends Component {
 
     state = {
-        options :[], // 用来显示级联列表的数组
-        }
+        options: [], // 用来显示级联列表的数组
+    }
 
-    initOptions=(categorys)=>{
+    initOptions=async(categorys)=>{
         //根据categorys数组生成options数组
         const options = categorys.map(c=>({
             value: c._id,
             label: c.name,
             isLeaf: false,
         }))
+
+        //如果是一个二级分类商品的更新
+        const {isUpade,product} = this;
+        const {pCategoryId,categoryId} = product;
+        if(isUpade && pCategoryId!=='0'){
+            //获取对应的二级分类列表
+            const subCategorys = await this.getCategorys(pCategoryId)
+            //生成二级下拉列表的options
+            const cOptions = subCategorys.map(c=>({
+                value: c._id,
+                label: c.name,
+                isLeaf: true,
+            }))
+            //找到当前商品对应的一级option对象
+            const targetOption = options.find(option=>option.value===pCategoryId)
+
+            //关联到对应的一级option上
+            targetOption.children = cOptions
+        }
         //更新options状态
         this.setState({
             options
         })
     }
-
+    
     //异步获取一级/二级分类列表
-    getCategorys= async (parentId)=>{
+    getCategorys = async (parentId) => {
         const result = await reqCategorys(parentId)
-        if(result.status===0){
+        if (result.status === 0) {
             const categorys = result.data
-            if(parentId==='0'){
+            if (parentId === '0') {
                 this.initOptions(categorys)
-            }else{
+            } else {
                 return categorys
             }
         }
@@ -40,108 +59,139 @@ class ProductAddUpdate extends Component {
 
 
     loadData = async selectedOptions => {
-        const targetOption = selectedOptions[0];
+        const targetOption = selectedOptions[selectedOptions.length - 1];
         targetOption.loading = true;
 
         //根据选中的分类，请求获取二级分类列表
         const subCategorys = await this.getCategorys(targetOption.value)
         targetOption.loading = false;
-        if(subCategorys && subCategorys.length>0){
+        if (subCategorys && subCategorys.length > 0) {
             //生成一个二级options
-            const childOptions = subCategorys.map(c=>({
+            const childOptions = subCategorys.map(c => ({
                 value: c._id,
                 label: c.name,
                 isLeaf: true,
             }))
             //关联到二级option上
             targetOption.children = childOptions
-        }else{
-            targetOption.isLeaf=true 
+        } else {
+            targetOption.isLeaf = true
         }
+
+        // 更新 options 状态
+        this.setState({
+            options: [...this.state.options],
+        });
     };
 
-    submit=()=>{
-        this.props.form.validateFields((err,value)=>{
-            if(!err){
-                
+    submit = () => {
+        this.props.form.validateFields((err, value) => {
+            if (!err) {
+
             }
         })
     }
 
-    
 
-    validatorPrice=(rule, value, callback)=>{
-        if(value*1 > 0){
+
+    validatorPrice = (rule, value, callback) => {
+        if (value * 1 > 0) {
             callback()
-        }else{
+        } else {
             callback('价格必须大于0')
         }
     }
 
-    componentDidMount(){
+    componentDidMount() {
         this.getCategorys('0')
     }
 
-    render() { 
+    componentWillMount() {
+        const product = this.props.location.state;
+        this.isUpade = !!product;
+        this.product = product || {}
+    }
 
-        const formItemLaypout ={
-            labelCol: {span:2},
-            wrapperCol: {span:8}
+    render() {
+
+        const { isUpade, product } = this
+
+        const formItemLaypout = {
+            labelCol: { span: 2 },
+            wrapperCol: { span: 8 }
         };
 
         const title = (
             <span>
-                <LinkButton>
-                    <Icon type="arrow-left" style={{fontSize:20}} />
-                    <span>添加商品</span>
+                <LinkButton onClick={() => this.props.history.goBack()}>
+                    <Icon type="arrow-left" style={{ fontSize: 20 }} />
+                    <span>{isUpade ? '修改商品' : '添加商品'}</span>
                 </LinkButton>
             </span>
         )
 
-        const {getFieldDecorator} = this.props.form
+        const { getFieldDecorator } = this.props.form
 
-        
+        const categoryIds = []
+        const { pCategoryId, categoryId } = product
+        if (isUpade) {
+            if (pCategoryId === '0') {
+                categoryIds.push(categoryId)
+            } else {
+                categoryIds.push(pCategoryId)
+                categoryIds.push(categoryId)
+            }
+        }
 
-        return ( 
+
+        return (
             <Card title={title}>
                 <Form {...formItemLaypout}>
                     <Item label='商品名称'>
-                    {
-                        getFieldDecorator('name',{
-                            initialValue:'',
-                            rules:[
-                                {required: true, message: '必须输入商品名称'}
-                            ]
-                        })(<Input placeholder='请输入商品名称'/>)
-                    }
+                        {
+                            getFieldDecorator('name', {
+                                initialValue: product.name,
+                                rules: [
+                                    { required: true, message: '必须输入商品名称' }
+                                ]
+                            })(<Input placeholder='请输入商品名称' />)
+                        }
                     </Item>
 
                     <Item label='商品描述'>
-                    {
-                        getFieldDecorator('desc',{
-                            initialValue:'',
-                            rules:[
-                                {required: true, message: '必须输入商品描述'}
-                            ]
-                        })(<TextArea placeholder='请输入商品描述' autosize/>)
-                    }
+                        {
+                            getFieldDecorator('desc', {
+                                initialValue: product.desc,
+                                rules: [
+                                    { required: true, message: '必须输入商品描述' }
+                                ]
+                            })(<TextArea placeholder='请输入商品描述' autosize />)
+                        }
                     </Item>
                     <Item label='商品价格'>
-                    {
-                        getFieldDecorator('price',{
-                            initialValue:'',
-                            rules:[
-                                {required: true, message: '必须输入商品价格'},
-                                {validator: this.validatorPrice}
-                            ]
-                        })(<Input type='number' placeholder='请输入商品价格' addonAfter='元'/>)
-                    }
+                        {
+                            getFieldDecorator('price', {
+                                initialValue: product.price,
+                                rules: [
+                                    { required: true, message: '必须输入商品价格' },
+                                    { validator: this.validatorPrice }
+                                ]
+                            })(<Input type='number' placeholder='请输入商品价格' addonAfter='元' />)
+                        }
                     </Item>
                     <Item label='商品分类'>
-                    <Cascader
-                        options={this.state.options}
-                        loadData={this.loadData}
-                    />
+                        {
+                            getFieldDecorator('categoryIds', {
+                                initialValue: categoryIds,
+                                rules: [
+                                    { required: true, message: '必须输入商品分类' },
+                                ]
+                            })(<Cascader
+                                options={this.state.options}
+                                loadData={this.loadData}
+                            />
+                            )
+                        }
                     </Item>
                     <Item label='商品图片'>
                         <div></div>
@@ -154,8 +204,8 @@ class ProductAddUpdate extends Component {
                     </Item>
                 </Form>
             </Card>
-         );
+        );
     }
 }
- 
-export default Form.create()(ProductAddUpdate) ;
+
+export default Form.create()(ProductAddUpdate);
