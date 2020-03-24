9fbac0320b741fe70be5f85164edfb710d4b2453
YufaZhu <412167563@qq.com>
2020-03-24 21:40:13 +0800

    role

--- src/api/index.js
+++ src/api/index.js
@@ -21,32 +21,56 @@ export const reqWeather=(city)=> {
         })
     })
 }
+// export const reqWeather=(city)=>{
+//     const url =
+//         `http://api.map.baidu.com/telematics/v3/weather?location=${city}&output=json&ak=3p4
+//     9MVra6urFRGOT9s8UBWr2`
+//     return new Promise((resolve, reject) => {
+//         jsonp(url, {
+//             param: 'callback'
+//         }, (error, response) => {
+//             if (!error && response.status === 'success') {
+//                 const { dayPictureUrl, weather } = response.results[0].weather_data[0]
+//                 resolve({ dayPictureUrl, weather })
+//             } else {
+//                 alert('获取天气信息失败')
+//             }
+//         })
+//     })
+// }
 
 //获得一级/二级分类列表
-export const reqCategorys = (parentId)=>ajax('/manage/category/list',{parentId});
+export const reqCategorys = (parentId) => ajax('/manage/category/list', { parentId });
 //添加分类
-export const reqAddCategory = (categoryName,parentId)=>ajax('/manage/category/add',{categoryName,parentId},'POST')
+export const reqAddCategory = (categoryName, parentId) => ajax('/manage/category/add', { categoryName, parentId }, 'POST')
 //更新分类   ({categoryId,categoryName}),(categoryId,categoryName),参数里面这两种写法都可以，
 //前一种是对象，取出结果需要解构，后者直接就是参数
-export const reqUpdateCategory = ({categoryId,categoryName})=>ajax('/manage/category/update',{categoryId,categoryName},'POST')
+export const reqUpdateCategory = ({ categoryId, categoryName }) => ajax('/manage/category/update', { categoryId, categoryName }, 'POST')
 
 //获取商品分页列表
-export const reqProducts = (pageNum,pageSize) =>ajax('/manage/product/list',{pageNum,pageSize})
+export const reqProducts = (pageNum, pageSize) => ajax('/manage/product/list', { pageNum, pageSize })
 
 //根据名称/描述搜索商品
-export const reqSearchProducts = ({pageNum,pageSize,searchType,searchName})=>ajax('/manage/product/search',
-    {pageNum,
-    pageSize,
-    [searchType]:searchName})
+export const reqSearchProducts = ({ pageNum, pageSize, searchType, searchName }) => ajax('/manage/product/search',
+    {
+        pageNum,
+        pageSize,
+        [searchType]: searchName
+    })
 
 //根据分类Id获取分类
-export const reqCategory = (categoryId)=>ajax('/manage/category/info',{categoryId});
+export const reqCategory = (categoryId) => ajax('/manage/category/info', { categoryId });
 
 //更新商品的状态（上架或下架的处理）
-export const reqUpdateProductStatus = (productId, status) =>ajax('/manage/product/updateStatus', {productId,status}, 'POST')
+export const reqUpdateProductStatus = (productId, status) => ajax('/manage/product/updateStatus', { productId, status }, 'POST')
 
 //删除图片
-export const reqDeleteImg = (name)=>ajax('/manage/img/delete',{name}, 'POST')
+export const reqDeleteImg = (name) => ajax('/manage/img/delete', { name }, 'POST')
 
 //添加/更新商品
-export const reqAddOrUpdateProduct =(product)=>ajax('/manage/product/'+(product._id?'update':'add'),product,'POST')
+export const reqAddOrUpdateProduct = (product) => ajax('/manage/product/' + (product._id ? 'update' : 'add'), product, 'POST')
+
+// 添加角色
+export const reqAddRole = (roleName) => ajax('/manage/role/add', { roleName }, 'POST')
+// 获取角色列表
+export const reqRoles = () => ajax('/manage/role/list')
--- src/pages/role/role.jsx
+++ src/pages/role/role.jsx
@@ -1,14 +1,159 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import {
+    Card,
+    Button,
+    Table,
+    Modal,
+    message
+} from 'antd'
+import { PAGE_SIZE } from "../../utils/constants"
+import { reqRoles, reqAddRole, reqUpdateRole } from '../../api'
+import AddForm from './add-form'
 
 class Role extends Component {
-   
-    render() { 
-        return ( 
-            <div>
-                Role
-            </div>
-         );
+
+    state = {
+        roles: [], // 所有角色的列表
+        role: {}, // 选中的 role
+        isShowAdd: false, // 是否显示添加界面
+    }
+
+    initColumn = () => {
+        this.columns = [
+            {
+                title: '角色名称',
+                dataIndex: 'name'
+            },
+            {
+                title: '创建时间',
+                dataIndex: 'create_time',
+                //render: (create_time) => formateDate(create_time)
+            },
+            {
+                title: '授权时间',
+                dataIndex: 'auth_time',
+                //render: formateDate
+            },
+            {
+                title: '授权人',
+                dataIndex: 'auth_name'
+            },
+        ]
+    }
+
+
+    onRow = (role) => {
+        return {
+            onClick: event => { // 点击行
+                console.log('row onClick()', role)
+                // alert('点击行')
+                this.setState({
+                    role
+                })
+            },
+        }
+    }
+
+    getRoles = async () => {
+        const result = await reqRoles()
+        if (result.status === 0) {
+            const roles = result.data
+            this.setState({
+                roles
+            })
+        }
+    }
+
+    addRole = () => {
+        this.form.validateFields(async (error, values) => {
+            if (!error) {
+                // 隐藏确认框
+                this.setState({
+                    isShowAdd: false
+                })
+                // 收集输入数据
+                const { roleName } = values
+                this.form.resetFields()
+                // 请求添加
+                const result = await reqAddRole(roleName)
+                // 根据结果提示/更新列表显示
+                if (result.status === 0) {
+                    message.success('添加角色成功')
+                    // this.getRoles()
+                    // 新产生的角色
+                    const role = result.data
+                    // 更新 roles 状态
+                    /*const roles = this.state.roles
+                    roles.push(role)
+                    this.setState({
+                    roles
+                    })*/
+                    // 更新 roles 状态: 基于原本状态数据更新
+                    this.setState(state => ({
+                        roles: [...state.roles, role]
+                    }))
+                } else {
+                    message.success('添加角色失败')
+                }
+            }
+        })
+    }
+
+
+    componentWillMount() {
+        this.initColumn()
+    }
+    componentDidMount() {
+        this.getRoles()
+    }
+
+    render() {
+
+        const { roles, role, isShowAdd, } = this.state
+        const title = (
+            <span>
+                <Button type='primary' onClick={() => this.setState({ isShowAdd: true })}>创建角色</Button> &nbsp;&nbsp;
+                <Button type='primary' disabled={!role._id}>设置角色权限</Button>
+            </span>
+        )
+
+        return (
+            <Card title={title}>
+                <Table
+                    bordered
+                    rowKey='_id'
+                    dataSource={roles}
+                    columns={this.columns}
+                    pagination={{ defaultPageSize: PAGE_SIZE }}
+                    rowSelection={{ type: 'radio', selectedRowKeys: [role._id] }}
+                    onRow={this.onRow}
+                />
+                <Modal
+                    title="添加角色"
+                    visible={isShowAdd}
+                    onOk={this.addRole}
+                    onCancel={() => {
+                        this.setState({ isShowAdd: false })
+                        this.form.resetFields()
+                    }}
+                >
+                    <AddForm
+                        setForm={(form) => this.form = form}
+                    />
+                </Modal>
+                {/* <Modal
+                    title="设置角色权限"
+                    visible={isShowAuth}
+                    onOk={this.updateRole}
+                    onCancel={() => {
+                        this.setState({ isShowAuth: false })
+                    }}
+                >
+                    <AuthForm ref={this.auth} role={role} />
+                </Modal> */}
+            </Card>
+        )
     }
 }
- 
+
 export default Role;
