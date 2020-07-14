ae44eddd7ba0896315b13441278bca16414e338b
Yufa Zhu <51221129+YufaZhu@users.noreply.github.com>
2020-07-14 15:09:32 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import storageUtils from './utils/storageUtils'
 import memoryUtils from './utils/memoryUtils'
+import {Provider} from 'react-redux'
+import store from './redux/store'
 // 如果 本地 保存了 user, 将 user 保存到内存中
 const user = storageUtils.getUser()
 if (user && user._id) {
     memoryUtils.user = user
 }
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Prodover store={store}>
+        <App />
+    </Prodover>
+    , document.getElementById('root'));
 
 
