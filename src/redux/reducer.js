75c8b027c67e120c09a5f2b39e9485a0ae011aa2
Yufa Zhu <51221129+YufaZhu@users.noreply.github.com>
2020-07-14 15:17:08 +0800

    Update reducer.js

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,1 +1,30 @@
+import {SET_HEADER_TITLE} from './action-type'
+import storageUtils from '../utils/storageUtils'
+import {combineReducers} from 'redux'
 
+//用来管理头部标题的reducer函数
+const initHeaderTitle = '首页'
+function headerTitle(state=initHeaderTitle,action){
+    switch(action.type){
+        case SET_HEADER_TITLE:
+            return action.data
+        default:
+            return state
+    }
+}
+
+//用来管理当前登录用户的reducer函数
+const initUser = storageUtils.getUser()
+function user(state=initUser,action){
+    switch(action.type){
+        case INCREMENT:
+            return state + action.data
+        default:
+            return state
+    }
+}
+
+export default combineReducers({
+    headerTitle,
+    user
+})
